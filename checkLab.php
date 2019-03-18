6254e557d04fc5673ad6960a99f820767b7eef74
Ross Jubber <ross.jubber@student.manchester.ac.uk>
2019-03-18 23:25:48 +0000

    Added query

--- checkLab.php
+++ checkLab.php
@@ -34,6 +34,20 @@ $lab = $_POST['selected_lab'];
 
 echo "You have selected lab $lab";
 
+//do this part for all - free computers
+$query = mysqli_query($con, "SELECT * FROM compStatus");
+while ($query_row = mysqli_fetch_assoc($query)){
+    $computerID = $query_row['computerID'];
+    $labs = $query_row['labs'];
+    $labs_array = explode(',', $labs);
+    echo "Comp number = $computerID";
+    echo "LF31 = $labs_array[0]";
+    echo "TOOTILL 1 = $labs_array[1]";
+    echo "TOOTILL 0 = $labs_array[2]";
+}
+
+//do this part only if they are an admin - show people that shouldnt be there
+
 
 //if lab == a database lab
 ///////////////////$query = mysqli_query($con, );
