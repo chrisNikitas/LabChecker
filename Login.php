e30043875df704358b4c7cb4daafb3d3593de2c2
Ross Jubber <ross.jubber@student.manchester.ac.uk>
2019-03-19 12:37:20 +0000

    Initial commit

---
+++ Login.php
@@ -0,0 +1,31 @@
+ <?php
+        //function login() {
+         //$database_host = "dbhost.cs.man.ac.uk";
+         //$database_user = "[insert username]";
+         //$database_pass = "[insert password]";
+         //$group_dbNames = array("2018_comp10120_w4",);
+         require ("config.inc.php");
+         $dbConn = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[0]);
+         if ($dbConn->connect_error) {
+          die("Connection failed: " . $dbConn->connect_error);
+         }
+         else {
+          $inputUsername = $_POST[Username];
+          $inputPassword = $_POST[Password];
+          $sqlQuery = "SELECT Teacher, Lab Group FROM Account WHERE Username = '$inputUsername' AND Password = '$inputPassword'";
+          $result = $conn->query(sqlQuery);
+          if ($result->num_rows > 0) {
+           $_SESSION['usernameLog'] = $result["Username"];
+           $_SESSION['passwordLog'] = $result["Password"];
+           $_SESSION['teacherBool'] = $result["Teacher"];
+           $_SESSION['labGroup'] = $result["Lab Group"];
+           header("Location: index.php");
+           }
+          else {
+           $_POST[noResults] = "Username or password is wrong";
+           echo "<script type='text/javascript'>alert(noResults);</script>";
+           $dbConn->close();
+           header("Location: Login.html");
+          }
+        }
+?>
