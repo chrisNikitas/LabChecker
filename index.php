868b999963cea4ca3b7b7b3e95e8c13a8f0de84d
Ross Jubber <ross.jubber@student.manchester.ac.uk>
2019-03-19 02:54:49 +0000

    Making form automatically submit

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!--<script src="checkLab.js?2"></script>-->
+<script src="checkLab.js?2"></script>
 <script>
 
 	function SHOW_LF31() {
@@ -91,13 +91,10 @@
 
 
   function getData() {
-    //alert("This loop is running!");
-		//$('list_form').trigger();
+		$('list_form').trigger();
 		//document.forms["list_form"].submit();
 
 	  alert("This loop is running!");
-
-    //^^^^running left column stuff automatically
 
 
 
