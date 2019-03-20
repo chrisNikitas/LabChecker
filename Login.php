cd232daff02d9a9d1abca34ea0102391fffb6917
Ross Jubber <ross.jubber@student.manchester.ac.uk>
2019-03-20 13:19:08 +0000

    Fixing login page and image refresh

--- Login.php
+++ Login.php
@@ -5,28 +5,31 @@
          //$database_pass = "[insert password]";
          //$group_dbNames = array("2018_comp10120_w4",);
          require ("config.inc.php");
-         $dbConn = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[0]);
+         //$dbConn = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[0]);
+         $dbConn = mysqli_connect($database_host, $database_user, $database_pass, $group_dbnames[0]);
          if ($dbConn->connect_error) {
-          die("Connection failed: " . $dbConn->connect_error);
+           die("Connection failed: " . $dbConn->connect_error);
          }
          else {
-          $inputUsername = $_POST['Username'];
-          $inputPassword = $_POST['Password'];
-          $sqlQuery = "SELECT Teacher, Lab Group FROM Account WHERE Username = '$inputUsername' AND Password = '$inputPassword'";
-          $result = $dbConn->query($sqlQuery);
-          if ($result->num_rows > 0) {
-           session_start();
-           $_SESSION['usernameLog'] = $result["Username"];
-           $_SESSION['passwordLog'] = $result["Password"];
-           $_SESSION['teacherBool'] = $result["Teacher"];
-           $_SESSION['labGroup'] = $result["Lab Group"];
-           header("Location: index.php");
+           $inputUsername = $_POST['Username'];
+           $inputPassword = $_POST['Password'];
+           /*$sqlQuery = "SELECT * FROM Account WHERE Username = '$inputUsername' AND Password = '$inputPassword'";
+           $result = $dbConn->query($sqlQuery);*/
+           $query = mysqli_query($con, "SELECT * FROM Users WHERE Username = '$inputUsername' AND Password = '$inputPassword'");
+           //if ($result->num_rows > 0) {
+           if (mysqli_num_rows($query) > 0) {
+             session_start();
+             $_SESSION['usernameLog'] = $result["Username"];
+             $_SESSION['passwordLog'] = $result["Password"];
+             $_SESSION['teacherBool'] = $result["Teacher"];
+             $_SESSION['labGroup'] = $result["LabGroup"];
+             header("Location: index.php");
            }
-          else {
-           $noResults = "Username or password is wrong";
-           echo '<script type="text/javascript">alert("Username or password is wrong");</script>';
-           $dbConn->close();
-           //header("Location: Login.html");
-          }
-        }
+           else {
+             $noResults = "Username or password is wrong";
+             echo '<script type="text/javascript">alert("Username or password is wrong");</script>';
+             $dbConn->close();
+             //header("Location: Login.html");
+           }
+         }
 ?>
