32b0616165090f540b86699a434d8434c73f7821
Xiaotan Zhu <xiaotan.zhu@student.manchester.ac.uk>
2019-03-22 12:45:46 +0000

    cache-control

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ else {
 
 <html>
 <head>
-	<meta name="viewport" http-equiv="Pragma" content="width=device-width, initial-scale=1, no-cache">
+	<meta name="viewport" http-equiv="cache-control" content="width=device-width, initial-scale=1, no-cache">
 	<title>Labchecker</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
