a30e1f083973e961cf6d984cdc5aa2f10b05e4a2
Xiaotan Zhu <xiaotan.zhu@student.manchester.ac.uk>
2019-03-22 12:51:49 +0000

    header dis cache

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
 <?php
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
 $user_display = "Error identifying user";
 
 session_start();
